6ca50ada9bd1c0553d7e046944c387a00abfcb9c
Ashton Hull <ashton.hull2022@gmail.com>
2026-04-15 15:24:14 -0500

    minor changes to fix expo go

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,14 +2,22 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/types/database.types';
 
+const isServer = typeof window === 'undefined';
+
+const noopStorage = {
+  getItem: () => Promise.resolve(null),
+  setItem: () => Promise.resolve(),
+  removeItem: () => Promise.resolve(),
+};
+
 export const supabase = createClient<Database>(
   process.env.EXPO_PUBLIC_SUPABASE_URL!,
   process.env.EXPO_PUBLIC_SUPABASE_KEY!,
   {
     auth: {
-      storage: AsyncStorage,
-      autoRefreshToken: true,
-      persistSession: true,
+      storage: isServer ? noopStorage : AsyncStorage,
+      autoRefreshToken: !isServer,
+      persistSession: !isServer,
       detectSessionInUrl: false,
     },
   })
